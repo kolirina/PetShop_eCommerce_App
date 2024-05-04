a185f8594d496059c2b7430e868effff5b16f19f
veta306 <103876674+veta306@users.noreply.github.com>
2024-05-04 20:30:07 +0300

    feat: RSS-ECOMM-1_08-1_16 (#2)
    * feat: RSS-ECOMM-1_08 set up bundler
    
    * feat: RSS-ECOMM-1_09 configure typescript
    
    * feat: RSS-ECOMM-1_10 configure eslint
    
    * feat: RSS-ECOMM-1_11 configure prettier
    
    * feat: RSS-ECOMM-1_12 configure husky
    
    * feat: RSS-ECOMM-1_13 install vitest
    
    * feat: RSS-ECOMM-1_14-1_16 create scripts
    
    * feat: add commit-msg hook for husky
    
    * fix: add corrections to configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: [".eslintrc.cjs", "vite.config.js"],
+};
