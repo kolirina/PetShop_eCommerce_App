23a5b5ad7d13f6d05290660a827319493d798ae6
veta306 <vv097099@gmail.com>
2024-05-16 01:00:06 +0300

    feat: add styles for header

--- src/components/footer/index.ts
+++ src/components/footer/index.ts
@@ -1,9 +1,13 @@
+import { createSpan } from '../../utils/elementCreator';
+import styles from './footer.module.css';
+
 export default class Footer {
   private container: HTMLElement;
 
   constructor() {
     this.container = document.createElement('footer');
-    this.container.textContent = 'Footer Content';
+    createSpan(styles.info, 'Ⓒ2024 Pet Shop', this.container);
+    this.container.classList.add(styles.footer);
   }
 
   getFooterElement() {
--- src/pages/templatePage/index.ts
+++ src/pages/templatePage/index.ts
@@ -2,6 +2,7 @@ import Footer from '../../components/footer';
 import Header from '../../components/header';
 import Router from '../../router';
 import Page from '../Page';
+import styles from './templatePage.module.css';
 
 export default class TemplatePage extends Page {
   private main: HTMLElement;
@@ -9,6 +10,7 @@ export default class TemplatePage extends Page {
   constructor(router: Router, parentElement: HTMLElement) {
     super(router, parentElement);
     this.main = document.createElement('main');
+    this.container.classList.add(styles.page);
     this.container.append(
       new Header(router).getHeaderElement(),
       this.main,
